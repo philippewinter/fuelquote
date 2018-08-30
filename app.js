26bc2675fec8d46e8879458f04ee5e182ec01f9f
Philippe Winter <philippe.winter@gmail.com>
2018-08-30 15:11:25 -0300

    handlebars

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 const express = require('express');
-const path = require('path');
+const exphbs  = require('express-handlebars');
+
 //Init app
 const app = express();
+const port = 3000;
 
-//Load view
-app.set('views', path.join(__dirname,'views'));
-app.set('view engine', 'pug');
+//Handlebar Middleware
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
 
 //Home route
-app.get('/pages', function (req, res) {
+app.get('/', function (req, res) {
     res.render('new_flight');
   });
 
   //Start Server
-  app.listen(3000, () => console.log('Server started on port 3000!'));
+  app.listen(port, () => console.log(`Server started on port ${port}!`));
