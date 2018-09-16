fe81516a65ff69b62452c1be99c15ef44a1f4bcb
Philippe Winter <philippe.winter@gmail.com>
2018-09-16 22:27:50 +0200

    body-parser e schema

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 //Init app
 const app = express();
@@ -8,9 +9,18 @@ const port = 3000;
 
 //Connect to Mogoose
 mongoose
-  .connect('mongodb://localhost/aviation')
+  .connect(
+    'mongodb://localhost/aviation',
+    {
+      useNewUrlParser: true
+    }
+  )
   .then(() => console.log('MognoDB connected...'))
   .catch(err => console.log(err));
+
+//Load Quote Model
+require('./models/Quote');
+const Quote = mongoose.model('quote');
 
 //Bower
 app.use(express.static(__dirname + '/public'));
@@ -19,9 +29,30 @@ app.use(express.static(__dirname + '/public'));
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
+//BodyParser Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 //Home route
 app.get('/', function(req, res) {
   res.render('new_flight');
+});
+
+//Process Form
+app.post('/', (req, res) => {
+  console.log(req.body);
+
+  const newUser = {
+    dt_flight: req.body.dt_flight,
+    acft: req.body.acft,
+    origin: req.body.origin,
+    destination: req.body.destination,
+    gal: req.body.gal,
+    fbo: req.body.fbo
+  };
+  new Quote(newUser).save().then(quote => {
+    res.redirect('/quotes');
+  });
 });
 
 //Start Server
---
+++ models/Quote.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//Create Schema
+const QuoteSchema = new Schema({
+  dt_quote: {
+    type: Date,
+    default: Date.now
+  },
+  dt_flight: {
+    type: Date,
+    required: true
+  },
+  acft: {
+    type: String,
+    required: true
+  },
+  pilot: {
+    type: String
+  },
+  origin: {
+    type: String,
+    required: true
+  },
+  destination: {
+    type: String,
+    required: true
+  },
+  gal: {
+    type: Number
+  },
+  fbo: {
+    type: String
+  },
+  provider: {
+    type: String
+  },
+  price: {
+    type: Number
+  }
+});
+
+mongoose.model('quote', QuoteSchema);
